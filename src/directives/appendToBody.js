f9c83f226b18e852b071aaa69bcabf698b6b2fab
Aviram Azoulay <aviram@monolith.co.il>
2020-12-30 10:48:56 +0200

    Improvements to appendToBody directive to support resizing & observing input changes to recalc its position

--- src/directives/appendToBody.js
+++ src/directives/appendToBody.js
@@ -1,6 +1,5 @@
-export default {
-    inserted (el, bindings, {context}) {
-        if (context.appendToBody) {
+function calculateBodyPosition(el, bindings, context) {
+    console.log('contextTest @ calculateBodyPosition', context);
             const {height, top, left, width} = context.$refs.toggle.getBoundingClientRect();
             let scrollX = window.scrollX || window.pageXOffset;
             let scrollY = window.scrollY || window.pageYOffset;
@@ -9,7 +8,17 @@ export default {
                 left: (scrollX + left) + 'px',
                 top: (scrollY + top + height) + 'px',
             });
-            
+}
+
+export default {
+    inserted (el, bindings, {context}) {
+        if (context.appendToBody) {
+            el.resizeFn = () => {
+                calculateBodyPosition(el, bindings, context);    
+            };
+            calculateBodyPosition(el, bindings, context);
+            context.$on('input', el.resizeFn);
+            window.addEventListener('resize', el.resizeFn);
             document.body.appendChild(el);
         }
     },
@@ -18,6 +27,9 @@ export default {
         if (context.appendToBody) {
             if (el.unbindPosition && typeof el.unbindPosition === 'function') {
                 el.unbindPosition();
+            }
+            if( el.resizeFn ) {
+                window.removeEventListener('resize', el.resizeFn);
             }
             if (el.parentNode) {
                 el.parentNode.removeChild(el);
